5cfe5420d06ea2d95f8e90f7aa65973be0818551
EricZhao <qyzhaojinxi@163.com>
2014-11-20 23:39:02 +0800

    Initial Commit

---
+++ AutoLayoutLearn/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoLayoutLearn
+//
+//  Created by 赵进喜 on 14/11/20.
+//  Copyright (c) 2014年 EricZhao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
